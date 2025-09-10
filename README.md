e5c55e11252ae2b113f58a7bdf7165561315edd5
Daniel Brownsey <danielbrownsey01@gmail.com>
2025-09-10 21:03:28 +0100

    chore: removed empty folder from inital setup.

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ REQUIREMENTS:
 🎯 Create a working GET API route in your server.
 🎯 Create a working POST API route in your client.
 🎯 Seed your database with realistic-looking ‘dummy’ data through the Supabase query editor or a seed file in your server. Ensure that this is saved and submitted (in a screenshot or seed file form) so it can be marked and tested efficiently.
+
+I used AI considerably less than previous assignments, and the actual process of troubleshooting felt much more satisfying. I'll ceratinly be using it much less frequently, but it is great for learning the name of the problem I need to google. When googling one of the problems before turning to AI, I saw an odd way to print the current date in a webpage that I've since lost (const today = new Date().toISOString().split("T")[0];).
+
+I did use this webpage to get date like this: const date = new Date().toLocaleDateString();
+
+https://bugfender.com/blog/javascript-date-and-time
--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,58 @@
-//get user data and send to server
+const guestbookContainer = document.getElementById("guestbook-container");
+//I wanted to rename guestbookContainer to guestbookOutput but it broke something and the troubleshooting was getting a bit much
 
-//TODO:::::::::::::::::::::::::::::::::::::::::::::
-//select html element with user's input
-//      add event
-//      event handler will:
-//      prevent default event.preventDefault();
-//      create template for user's data
-//      `INSERT INTO table $1 $2 $3
-//      fill in template
-// send data JSON to the server
-//n.b. while in dev the server uses local host, but when deploying, this needs to be replace with the server deployed url
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(guestbookContainer);
+  const formValues = Object.fromEntries(formData);
+  console.log(formValues);
+  fetch("http://localhost:8080/guestbook", {
+    //This url will need to be replaced with a live url once things are underway
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ formValues }),
+  });
+}
 
-//TODO:::::::::::::::::::::::::::::::::::::::::::::
-//get data from server AND display on page
-//connect client to the specific server route that gets data from supabase
-//insert into HTML; append child, "messages" section in the HTML
-//      in some sort of loop, create new html elements to display each piece of data
-//      and then append to DOM: overflow-y CSS; ABSOLUTE POSITIONING
+guestbookContainer.addEventListener("submit", handleSubmit);
+
+async function getGuestbookContent() {
+  const response = await fetch("http://localhost:8080/guestbook");
+  const guestBookEntries = await response.json();
+  console.log(guestBookEntries);
+  return guestBookEntries;
+}
+
+//TODO::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//            guestBookEntries is coming from the get request as "Row: /guestname", so the guestBookEntries.guestname can't get in there, and is returning null. You need to go into the get request in server.js and output the SQL query as json
+//^ This note was left as a reminder for the next time I opened the code. The problem was that I had wrapped my columns in my get SQL query in brackets. It returned the results as a weird pseudo-json, and removing the brackets gave 'referencable' json and fixed everything. Brackets haunt me
+
+getGuestbookContent().then((guestBookEntries) => {
+  for (let i = 0; i < guestBookEntries.length; i++) {
+    // console.log(date);
+    let guestName = guestBookEntries[i].guestname;
+    let guestDate = guestBookEntries[i].date;
+    let guestContent = guestBookEntries[i].content;
+    const guestMessageContainer = document.getElementById("guestbook-messages");
+    const guestMessageElement = document.createElement("div");
+    // guestMessageContainer2.content = guestName;
+    guestMessageElement.className = "guestbook-message-parent";
+
+    const guestMessageName = document.createElement("p");
+    const guestMessageDate = document.createElement("p");
+    const guestMessageContent = document.createElement("p");
+    guestMessageName.className = "guestbook-message-child-name";
+    guestMessageDate.className = "guestbook-message-child-date";
+    guestMessageContent.className = "guestbook-message-child-content";
+    guestMessageName.textContent = `Message from: ${guestName}`;
+    guestMessageDate.textContent = `Dated: ${guestDate}`;
+    guestMessageContent.textContent = `Saying: ${guestContent}`;
+
+    guestMessageContainer.appendChild(guestMessageElement);
+    guestMessageElement.appendChild(guestMessageName);
+    guestMessageElement.appendChild(guestMessageDate);
+    guestMessageElement.appendChild(guestMessageContent);
+  }
+});
+
+//:::::::::::::::: Set date-time
